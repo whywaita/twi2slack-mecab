b609bb197b3d4a7dddef50489374812ba83325cc
whywaita <whywaita@whywrite.it>
2017-02-16 02:02:21 +0900

    Post to Slack

--- main.go
+++ main.go
@@ -1,29 +1,81 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/ChimeraCoder/anaconda"
-	"github.com/joho/godotenv"
+	"github.com/nlopes/slack"
 	mecab "github.com/shogo82148/go-mecab"
 )
 
-func main() {
-	// load .env
-	err := godotenv.Load()
+func validationEnviroments() error {
+	if os.Getenv("TWITTER_CONSUMER_KEY") == "" {
+		return errors.New("TWITTER_CONSUMER_KEY is blank")
+	}
+
+	if os.Getenv("TWITTER_CONSUMER_SECRET") == "" {
+		return errors.New("TWITTER_CONSUMER_KEY is blank")
+	}
+
+	if os.Getenv("TWITTER_OAUTH_TOKEN") == "" {
+		return errors.New("TWITTER_OAUTH_TOKEN is blank")
+	}
+
+	if os.Getenv("TWITTER_OAUTH_TOKEN_SECRET") == "" {
+		return errors.New("TWITTER_OAUTH_TOKEN_SECRET is blank")
+	}
+
+	if os.Getenv("SLACK_TOKEN") == "" {
+		return errors.New("SLACK_TOKEN is blank")
+	}
+
+	if os.Getenv("SLACK_CHANNEL") == "" {
+		return errors.New("SLACK_CHANNEL is blank")
+	}
+
+	return nil
+}
+
+func postSlack(api *slack.Client, channelName string, message string) error {
+	params := slack.PostMessageParameters{}
+	attachment := slack.Attachment{
+		//Pretext: "pretext",
+		Text: message,
+	}
+
+	params.Attachments = []slack.Attachment{attachment}
+	channelID, timestamp, err := api.PostMessage(channelName, "Twitter Search Result", params)
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Printf("%s\n", err)
+		return err
+	}
+
+	log.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+	return nil
+}
+
+func main() {
+	var err error
+
+	// validation Enviroments value
+	err = validationEnviroments()
+	if err != nil {
+		log.Fatalf("Error: %s", err)
 	}
 
 	// set Twitter infomation
 	anaconda.SetConsumerKey(os.Getenv("TWITTER_CONSUMER_KEY"))
 	anaconda.SetConsumerSecret(os.Getenv("TWITTER_CONSUMER_SECRET"))
-
 	api := anaconda.NewTwitterApi(os.Getenv("TWITTER_OAUTH_TOKEN"), os.Getenv("TWITTER_OAUTH_TOKEN_SECRET"))
-	api.SetLogger(anaconda.BasicLogger) // set logger
+	api.SetLogger(anaconda.BasicLogger)
+
+	slackAPI := slack.New(os.Getenv("SLACK_TOKEN"))
+	slackChannel := os.Getenv("SLACK_CHANNEL")
 
 	// make mecab map
 	tagger, err := mecab.New(map[string]string{})
@@ -42,14 +94,17 @@ func main() {
 		case item := <-stream.C:
 			switch status := item.(type) {
 			case anaconda.Tweet:
-				// get Tweet
-				result, err := tagger.Parse(status.Text)
-				if err != nil {
-					panic(err)
+				if !strings.Contains(status.Text, "わいわいた") {
+				} else {
+					// get Tweet
+					result, err := tagger.Parse(status.Text)
+					if err != nil {
+						panic(err)
+					}
+					postSlack(slackAPI, slackChannel, result)
 				}
-
-				fmt.Println(result)
 			default:
+				// nothing
 			}
 		}
 	}
